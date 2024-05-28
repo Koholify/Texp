eb7033457fcad961d731516cce07b5b92b576fd1
Kyle Price <kounro211@gmail.com>
2024-05-28 16:01:32 -0230

    able to traverse filesystem

--- src/commandwindow.py
+++ src/commandwindow.py
@@ -22,23 +22,26 @@ class CommandWindow(NCWindow):
 
     def update(self):
         self.clear()
-        self.window.addstr(0, 0, CommandWindow.instructions, self.get_base_color())
-        if len(App.windows) > 0:
-            cur = App.windows[self.current_window]
-            self.window.addstr(1, 0, os.path.join(cur.dir, cur.get_current_item()), self.get_base_color())
+        try:
+            self.window.addstr(0, 0, CommandWindow.instructions, self.get_base_color())
+            if len(App.windows) > 0:
+                cur = App.windows[self.current_window]
+                relative = os.path.relpath(os.path.join(cur.dir, cur.get_current_item()), os.getcwd())
+                self.window.addstr(1, 0, relative, self.get_base_color())
+        except nc.error:
+            pass
         super().update()
 
     def move_dirs_up(self):
-        cd_old = os.getcwd()
+        cd_old = App.windows[1].dir
         spl_old = os.path.split(cd_old)
-        log.debug(f"new dir{spl_old}, {os.getcwd()}")
         select_old = App.windows[0].get_current_item()
         if spl_old[0] == "/":
             return
 
-        os.chdir(spl_old[0])
-        cd = os.getcwd()
-        log.debug(f"new dir2 {spl_old}, {os.getcwd()}")
+        #os.chdir(spl_old[0])
+        #cd = os.getcwd()
+        cd = spl_old[0]
         spl = os.path.split(cd)
         App.windows[0].set_dir(spl[0], spl[1])
         App.windows[1].set_dir(cd, select_old)
@@ -47,20 +50,18 @@ class CommandWindow(NCWindow):
         self.update()
 
     def move_dirs_down(self):
-        cd_old = os.getcwd()
-        select = App.windows[self.current_window].get_current_item()
+        current_window = App.windows[self.current_window]
+        log.debug(self.current_window)
+        cd_old = current_window.dir
+        select = current_window.get_current_item()
         if select == "" or select == None:
             return
 
         path = os.path.join(cd_old, select)
-        if not os.path.exists(path):
+        if not os.path.exists(path) or not os.path.isdir(path):
             return
 
-        st = os.stat(path)
-        if not stat.S_ISDIR(st.st_mode):
-            return
-
-        os.chdir(path)
+        #os.chdir(path)
         App.windows[0].set_dir(cd_old, select)
         App.windows[1].set_dir(path)
         App.windows[2].set_dir("")
--- src/dirwindow.py
+++ src/dirwindow.py
@@ -10,21 +10,26 @@ from ncwindowinput import NCWindowInput
 import logging as log
 import logs as _
 
+PERMISSION_DENIED = "PermissionDenied"
+
 class DirWindow(NCWindow):
     def __init__(self, size: tuple[int, int], pos: tuple[int, int], index: int, dir: str | None = None) -> None:
         super().__init__(size, pos)
         self.files: list[str] = []
         self.index = index
+        self.findex = 0
         if dir is not None:
             self.dir = dir
         else:
             self.dir = os.getcwd()
 
+        self.is_dir = os.path.isdir(self.dir)
         try:
             self.files = os.listdir(self.dir)
             self.files.sort()
         except:
             log.error(f"Unable to read dir: {self.dir}")
+            self.is_dir = False
 
         highlight = {
             stat.S_IFDIR: WC_HIGHLIGHT_DIR,
@@ -65,14 +70,22 @@ class DirWindow(NCWindow):
 
     def set_dir(self, path: str, pos: str = ""):
         self.dir = path
+        self.is_dir = False
         if self.dir == "" or self.dir == None:
             self.files = []
         elif os.path.isfile(self.dir):
             self.set_file_preview(self.dir)
             return
         else:
-            self.files = os.listdir(self.dir)
+            try:
+                self.files = os.listdir(self.dir)
+            except PermissionError:
+                self.set_preview(PERMISSION_DENIED)
+                self.is_dir = False
+                self.update()
+                return
 
+        self.is_dir = os.path.isdir(self.dir)
         self.files.sort()
 
         if pos == "" or pos == None:
@@ -83,6 +96,7 @@ class DirWindow(NCWindow):
 
     def set_file_preview(self, path: str):
         self.dir = path
+        self.is_dir = False
         if self.dir == "" or self.dir == None:
             self.files = []
         else:
@@ -94,6 +108,10 @@ class DirWindow(NCWindow):
                     self.files = ["Unable to preview file"]
             self.set_cursor(-1,0)
 
+    def set_preview(self, msg: str):
+        self.files = msg.split("\n")
+        self.set_cursor(-1,0)
+        self.update()
 
     def update(self):
         type = 0
@@ -104,9 +122,24 @@ class DirWindow(NCWindow):
             self.set_base_color(WC_DEFAULT)
 
         self.clear()
-        for i, file in enumerate(self.files):
+        files_to_show = []
+        cp = self.cursor_pos[0]
+        if len(self.files) <= self.height:
+            files_to_show = self.files
+        else:
+            log.debug(f"cp: {cp}")
+            if cp != -1:
+                if cp < self.findex:
+                    self.findex = cp
+                elif cp >= self.findex + self.height:
+                    self.findex = cp - self.height+1
+
+            for f in range(self.findex, max(self.findex + self.height, len(self.files))):
+                files_to_show.append(self.files[f])
+
+        for i, file in enumerate(files_to_show):
             i_type = type
-            if i == self.cursor_pos[0]:
+            if i + self.findex == cp:
                 i_type = 2
             color = WC_HIGHLIGHT if i_type == 2 else self.base_color
             try:
@@ -114,25 +147,47 @@ class DirWindow(NCWindow):
             except nc.error:
                 pass
 
-            path = os.path.join(self.dir, file)
-            if os.path.exists(path):
-                stats = os.stat(path)
-                color = self.get_line_color(stats, i_type)
+            if not self.is_dir:
+                try:
+                    self.window.addnstr(i, 0, file, self.width, nc.color_pair(color))
+                except nc.error:
+                    pass
             else:
-                color = self.base_color
-            try:
-                self.window.addnstr(i, 0, file, self.width, nc.color_pair(color))
-            except nc.error:
-                pass
+                path = os.path.join(self.dir, file)
+                if os.path.exists(path):
+                    stats = None
+                    try:
+                        stats = os.stat(path)
+                    except:
+                        stats = None
+                    color = self.get_line_color(stats, i_type)
+                else:
+                    log.debug(f"path not exist {path}")
+                    color = self.base_color
+                try:
+                    self.window.addnstr(i, 0, file, self.width, nc.color_pair(color))
+                except nc.error:
+                    pass
 
         super().update()
 
-    def get_line_color(self, result: os.stat_result, line_type: int = 0) -> int:
-        cmap = self.color_map[line_type]
+    def get_line_color(self, result: os.stat_result | None, line_type: int = 0) -> int:
+        cmap = None
+        try:
+            cmap = self.color_map[line_type]
+        except:
+            log.debug(f"Color map not found {line_type}")
+            pass
+
         if cmap is not None:
-            col = cmap[stat.S_IFMT(result.st_mode)]
-            if col is not None:
-                return col
+            if result is None:
+                return cmap[0]
+            else:
+                try:
+                    return cmap[stat.S_IFMT(result.st_mode)]
+                except KeyError:
+                    return cmap[0]
+
         return self.base_color
 
     def update_next_window_preview(self):
@@ -144,13 +199,17 @@ class DirWindow(NCWindow):
         if current is None or current == "":
             next_win.set_dir("")
         else:
-            st = os.stat(os.path.join(self.dir, current))
-            if stat.S_ISDIR(st.st_mode):
-                next_win.set_dir(os.path.join(self.dir, current))
-            elif stat.S_ISREG(st.st_mode):
-                next_win.set_file_preview(os.path.join(self.dir, current))
-            else:
+            try:
+                st = os.stat(os.path.join(self.dir, current))
+                if stat.S_ISDIR(st.st_mode):
+                    next_win.set_dir(os.path.join(self.dir, current))
+                elif stat.S_ISREG(st.st_mode):
+                    next_win.set_file_preview(os.path.join(self.dir, current))
+                else:
+                    next_win.set_dir("")
+            except PermissionError:
                 next_win.set_dir("")
+                next_win.set_preview(PERMISSION_DENIED)
 
         next_win.update_next_window_preview()
 
@@ -160,6 +219,11 @@ class DirWindowInput(NCWindowInput):
         self.window = window
 
     def default_handler(self, c: str, mods: int):
+        log.debug(f"inp: {self.window.dir}")
+        log.debug(self.window.is_dir)
+        if not self.window.is_dir:
+            return
+
         if c == "UPARROW" or c == "k":
             self.window.cursor_pos[0] -= 1
             self.clamp_index()
